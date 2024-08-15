9b4808324fc012676d1b2b662913d49138bf5718
DevMiguelAraujo <miguel.araujo.dev@gmail.com>
2024-08-15 11:50:14 -0300

    feat: 🎸 Basic functions of Accordion component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import Header from './header'
 import Accordion from './accordion'
 function App() {
+
+  const tutorialList = [`Clique em Gerar Piada para receber uma piada do Chuck Norris.`, `Você pode selecionar uma categoria da lista de categorias para receber piadas de uma categoria especifica.`, `Caso deseje ver outra piada da mesma categoria, clique no botão Gerar Novamente.`]
+
   return (
     <div className='flex flex-col justify-center text-center gap-y-6'>
       <Header/>
-      <Accordion/>
+      <Accordion titulo='Como Funciona?' lista={tutorialList} keyProp='passoTutorial_'/>
     </div>
   )
 }
--- src/accordion.tsx
+++ src/accordion.tsx
@@ -1,4 +1,36 @@
-function Accordion(){
-  return <div>Accordion a programar</div>
+import React, { useState } from "react";
+
+type AccordionProps = React.ComponentProps<"ul"> & {
+  titulo: string;
+  lista: string[];
+  keyProp: string;
+};
+
+function Accordion({ titulo, lista, keyProp }: AccordionProps) {
+  const [visible, setVisible] = useState(true);
+  const variantStyles = {
+    open: "max-h-fit bg-amber-200",
+    closed: "max-h-0 overflow-hidden",
+  };
+  return (
+    <div className="flex flex-col items-center text-start px-2">
+      <div>
+        <h2 className="text-lg font-semibold" onClick={() => setVisible(!visible)}>{titulo}</h2>
+        <ul
+          className={`${
+            visible ? variantStyles.open : variantStyles.closed
+          } transition-all duration-1000 px-2 py-2 flex flex-col gap-2`}
+        >
+          {lista.map((item, index) => (
+            <li key={`${keyProp}+${index}`}>
+              <p>
+                {item}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
-export default Accordion
+export default Accordion;
